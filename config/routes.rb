cf13e00e4f5da3c40dd67a59ea6ba2ff600e681e
Samantha <samantha.ming@gmail.com>
2016-02-07 02:28:56 -0800

    #3B: Implement CRUD for Comment resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,24 +3,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/about' => 'home#about'
 
-  # CRUD (Posts)
-
-  ## Create
-  get '/posts/new' => 'posts#new', as: :new_posts
-  post '/posts' => 'posts#create', as: :posts
-
-  ## Read
-  get '/posts' => 'posts#index'
-  get '/posts/:id' => 'posts#show', as: :post
-
-  ## Update
-  get '/posts/:id/edit' => 'posts#edit', as: :edit_post
-  patch '/posts/:id' => 'posts#update'
-
-  ## Destroy
-  delete '/posts/:id' => 'posts#Destroy'
-
-
-  # CRUD (Comments)
+  resources :posts
   resources :comments
 end
